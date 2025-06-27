0f9781ee699b0356320010e3da8ab1397e910b1e
Sindre Sømme <sindre.somme@kabal.com>
2025-06-27 22:10:00 +0200

    Walls work as is

---
+++ assets/scripts/wall.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+
+@export var move_speed: float = 200
+
+var target_position: Vector2
+var rotation_angle: float = 0.0
+
+func set_rotation_meta(vertical: bool, angle: float = 90.0, flipped: bool = false):
+	rotation_angle = 0.0
+	if vertical:
+		rotation_angle = angle
+	if flipped:
+		rotation_angle += 180.0
+	$Sprite2D.rotation = deg_to_rad(rotation_angle)
+	$Area2D.rotation = deg_to_rad(rotation_angle)
+
+func apply_meta():
+	var vertical = false
+	var angle = 90.0
+	var flipped = false
+	if has_meta("vertical"):
+		vertical = get_meta("vertical")
+	if has_meta("vertical_angle"):
+		angle = get_meta("vertical_angle")
+	if has_meta("flipped"):
+		flipped = get_meta("flipped")
+	set_rotation_meta(vertical, angle, flipped)
+
+func _ready() -> void:
+	apply_meta()
+	await get_tree().create_timer(10.0).timeout
+	queue_free()
+
+func _process(delta: float) -> void:
+	if target_position == null:
+		return
+	if $Area2D and $Area2D.get_overlapping_areas().size() > 0:
+		return
+	var dir = (target_position - position)
+	position += dir.normalized() * move_speed * delta
--- wall.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-var test : Big
-
-
-func _ready() -> void:
-	test = Big.new(5)
